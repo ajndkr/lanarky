564cfc357c6b885743da16dac563cdf111f974c0
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2023-04-07 07:56:26 +0200

    :fire: remove bad files

--- app/models.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class Message(BaseModel):
-    text: str
-    sender: str
-    timestamp: float
--- app/utils.py
+++
@@ -1,9 +0,0 @@
-# This file will contain utility functions for the chat application
-
-import datetime
-
-def get_current_time():
-    """
-    Returns the current time in ISO format
-    """
-    return datetime.datetime.now().isoformat()
