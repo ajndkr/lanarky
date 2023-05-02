caea563e6bbba2795ad3a0f9c5a5e6942dd37566
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2023-05-02 19:21:56 +0200

    :fire: remove template

--- fastapi_async_langchain/testing/__init__.py
+++ fastapi_async_langchain/testing/__init__.py
@@ -1,4 +1,3 @@
 from .gradio import mount_gradio_app
-from .templates import get_ws_template
 
-__all__ = ["mount_gradio_app", "get_ws_template"]
+__all__ = ["mount_gradio_app"]
--- fastapi_async_langchain/testing/settings.py
+++ fastapi_async_langchain/testing/settings.py
@@ -11,10 +11,6 @@ class Settings(BaseSettings):
     api_endpoint: str = "/chat"
     gradio_path: str = "/gradio"
 
-    # jinja2 template settings
-    websocket_url: str = "ws://localhost:8000"
-    websocket_endpoint: str = "/chat_ws"
-
     class Config:
         env_file = ".env"
 
