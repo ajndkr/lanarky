8b41c92ebaa2f9481594019f3d90c6ba10a0df84
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2023-05-02 09:55:05 +0200

    :zap: update RetrievalQAStreamingResponse

--- fastapi_async_langchain/responses/streaming/retrieval_qa.py
+++ fastapi_async_langchain/responses/streaming/retrieval_qa.py
@@ -5,6 +5,7 @@ from langchain.chains.retrieval_qa.base import BaseRetrievalQA
 from starlette.types import Send
 
 from ...callbacks import (
+    AsyncLLMChainStreamingCallback,
     AsyncRetrievalQAStreamingCallback,
     AsyncStreamingResponseCallback,
 )
@@ -35,8 +36,21 @@ class RetrievalQAStreamingResponse(BaseLangchainStreamingResponse):
                     )
                     break
             chain.combine_documents_chain.llm_chain.llm.callback_manager.add_handler(
+                AsyncLLMChainStreamingCallback(send=send)
+            )
+
+            if not isinstance(chain.callback_manager, AsyncCallbackManager):
+                raise TypeError(
+                    "chain.callback_manager must be an instance of AsyncCallbackManager"
+                )
+            for handler in chain.callback_manager.handlers:
+                if isinstance(handler, AsyncStreamingResponseCallback):
+                    chain.callback_manager.remove_handler(handler)
+                    break
+            chain.callback_manager.add_handler(
                 AsyncRetrievalQAStreamingCallback(send=send)
             )
+
             return await chain.acall(inputs)
 
         return wrapper
