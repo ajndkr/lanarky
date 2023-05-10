fe24273321a0e7be12c4d518bf2ebd77cfc9b06e
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2023-05-10 22:17:21 +0200

    :fire: remove websocket class
    will be added in a separate PR

--- fastapi_async_langchain/responses/conversational_retrieval.py
+++ fastapi_async_langchain/responses/conversational_retrieval.py
@@ -1,56 +1,22 @@
 from typing import Any, Awaitable, Callable, Dict, Union
 
-from langchain.callbacks import AsyncCallbackManager
 from langchain.chains.retrieval_qa.base import BaseRetrievalQA
 from starlette.types import Send
 
-from ..callbacks import (
-    AsyncLLMChainStreamingCallback,
-    AsyncRetrievalQAStreamingCallback,
-    AsyncStreamingResponseCallback,
-)
+from ..callbacks import AsyncRetrievalQAStreamingCallback
 from .base import BaseLangchainStreamingResponse
 
 
 class ConversationalRetrievalStreamingResponse(BaseLangchainStreamingResponse):
-    """BaseLangchainStreamingResponse class wrapper for BaseRetrievalQA instances."""
+    """BaseLangchainStreamingResponse class wrapper for ConversationalRetrievalStreamingResponse instances."""
 
     @staticmethod
     def _create_chain_executor(
         chain: BaseRetrievalQA, inputs: Union[Dict[str, Any], Any]
     ) -> Callable[[Send], Awaitable[Any]]:
         async def wrapper(send: Send):
-            if not isinstance(
-                chain.combine_docs_chain.llm_chain.llm.callback_manager,
-                AsyncCallbackManager,
-            ):
-                raise TypeError(
-                    "llm.callback_manager must be an instance of AsyncCallbackManager"
-                )
-            for (
-                handler
-            ) in chain.combine_docs_chain.llm_chain.llm.callback_manager.handlers:
-                if isinstance(handler, AsyncStreamingResponseCallback):
-                    chain.combine_docs_chain.llm_chain.llm.callback_manager.remove_handler(
-                        handler
-                    )
-                    break
-            chain.combine_docs_chain.llm_chain.llm.callback_manager.add_handler(
-                AsyncLLMChainStreamingCallback(send=send)
+            return await chain.acall(
+                inputs=inputs, callbacks=[AsyncRetrievalQAStreamingCallback(send=send)]
             )
-
-            if not isinstance(chain.callback_manager, AsyncCallbackManager):
-                raise TypeError(
-                    "chain.callback_manager must be an instance of AsyncCallbackManager"
-                )
-            for handler in chain.callback_manager.handlers:
-                if isinstance(handler, AsyncStreamingResponseCallback):
-                    chain.callback_manager.remove_handler(handler)
-                    break
-            chain.callback_manager.add_handler(
-                AsyncRetrievalQAStreamingCallback(send=send)
-            )
-
-            return await chain.acall(inputs)
 
         return wrapper
