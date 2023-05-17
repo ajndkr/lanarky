27e30b0b62a43128e4095e3a1936ba9798174ac4
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2023-05-18 01:51:22 +0200

    :wrench: update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,16 @@ release = "v0.6.1"
 
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.coverage",
+    "sphinx.ext.viewcode",
 ]
+
+# autodoc: Default to members and undoc-members
+autodoc_default_options = {"members": True}
+
+# autodoc: Don't inherit docstrings (e.g. for nn.Module.forward)
+autodoc_inherit_docstrings = False
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -25,5 +34,12 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_logo = "_static/logo_150px.png"
+html_theme = "furo"
 html_static_path = ["_static"]
+html_theme_options = {
+    "light_css_variables": {
+        "font-stack": "'Segoe UI', sans-serif",
+        "font-stack--monospace": "Courier, monospace",
+    },
+}
