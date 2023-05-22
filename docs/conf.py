517248cbe024296afefc420b0c7e266b97651a9d
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2023-05-22 16:40:49 +0200

    :arrow_up: bump patch version

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "Lanarky"
 copyright = "2023, Ajinkya Indulkar"
 author = "Ajinkya Indulkar"
-release = "v0.6.3"
+release = "v0.6.4"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
