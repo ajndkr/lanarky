e703fc5e6d2ceaca09e8417a35c0b481dea38dce
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2023-05-29 10:25:40 +0200

    :white_check_mark: update unit test

--- tests/routing/test_langchain_router.py
+++ tests/routing/test_langchain_router.py
@@ -1,7 +1,6 @@
 from unittest.mock import MagicMock
 
 import pytest
-from fastapi.params import Depends
 from langchain.chains import ConversationChain
 
 from lanarky.routing import LangchainRouter
@@ -12,21 +11,25 @@ def chain():
     return MagicMock(spec=ConversationChain)
 
 
-def test_langchain_router_init(chain):
-    router = LangchainRouter(langchain_object=chain)
+def test_langchain_router_init():
+    router = LangchainRouter()
 
-    assert router.langchain_object == chain
-    assert router.streaming_mode == 0
-    assert router.langchain_url == "/chat"
+    assert router.langchain_object is None
+    assert router.streaming_mode is None
+    assert router.langchain_url is None
     assert router.langchain_endpoint_kwargs == {}
-    assert isinstance(router.langchain_dependency, Depends)
+    assert router.langchain_dependencies == []
 
+
+def test_langchain_router_add_routes(chain):
     with pytest.raises(ValueError):
-        LangchainRouter(langchain_object=chain, streaming_mode=3)
+        LangchainRouter(langchain_url="/chat", langchain_object=chain, streaming_mode=3)
 
+    router = LangchainRouter()
 
-def test_langchain_router_base_route(chain):
-    router = LangchainRouter(langchain_object=chain)
+    router.add_langchain_api_route(
+        url="/chat", langchain_object=chain, streaming_mode=0
+    )
 
     assert len(router.routes) == 1
     assert router.routes[0].methods == {"POST"}
@@ -34,22 +37,22 @@ def test_langchain_router_base_route(chain):
     assert router.routes[0].response_model.schema()["title"] == "LangchainResponse"
     assert router.routes[0].body_field.type_.schema()["title"] == "LangchainRequest"
 
+    router.add_langchain_api_route(
+        url="/chat", langchain_object=chain, streaming_mode=1
+    )
 
-def test_langchain_router_streaming_route(chain):
-    router = LangchainRouter(langchain_object=chain, streaming_mode=1)
+    assert len(router.routes) == 2
+    assert router.routes[1].methods == {"POST"}
+    assert router.routes[1].path == "/chat"
+    assert router.routes[1].response_model is None
+    assert router.routes[1].body_field.type_.schema()["title"] == "LangchainRequest"
 
-    assert len(router.routes) == 1
-    assert router.routes[0].methods == {"POST"}
-    assert router.routes[0].path == "/chat"
-    assert router.routes[0].response_model is None
-    assert router.routes[0].body_field.type_.schema()["title"] == "LangchainRequest"
+    router.add_langchain_api_route(
+        url="/chat", langchain_object=chain, streaming_mode=2
+    )
 
-
-def test_langchain_router_streaming_json_route(chain):
-    router = LangchainRouter(langchain_object=chain, streaming_mode=2)
-
-    assert len(router.routes) == 1
-    assert router.routes[0].methods == {"POST"}
-    assert router.routes[0].path == "/chat"
-    assert router.routes[0].response_model is None
-    assert router.routes[0].body_field.type_.schema()["title"] == "LangchainRequest"
+    assert len(router.routes) == 3
+    assert router.routes[2].methods == {"POST"}
+    assert router.routes[2].path == "/chat"
+    assert router.routes[2].response_model is None
+    assert router.routes[2].body_field.type_.schema()["title"] == "LangchainRequest"
