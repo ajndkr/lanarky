89d063f58b81d3a83b74224685bf9d1e9b72dc34
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2023-05-30 20:44:43 +0200

    :zap: add `override` param to register

--- lanarky/register/base.py
+++ lanarky/register/base.py
@@ -2,7 +2,10 @@ from typing import Any, Optional, Union
 
 
 def register(
-    key: Union[list[str], str], _registry: dict[str, tuple[Any, list[str]]]
+    key: Union[list[str], str],
+    _registry: dict[str, tuple[Any, list[str]]],
+    *,
+    override: bool = False,
 ) -> Any:
     """Add a class/function to a registry with required keyword arguments.
 
@@ -13,6 +16,7 @@ def register(
     Args:
         key: key or list of keys to register the class/function under.
         _registry: registry to add the class/function to.
+        override: if True, override existing keys in the registry.
     """
 
     def _register_cls(cls: Any, required_kwargs: Optional[list] = None) -> Any:
@@ -22,7 +26,7 @@ def register(
             keys = key
 
         for _key in keys:
-            if _key in _registry:
+            if _key in _registry and not override:
                 raise KeyError(f"{cls} already registered as {_key}")
             _registry[_key] = cls if required_kwargs is None else (cls, required_kwargs)
         return cls
--- tests/test_register.py
+++ tests/test_register.py
@@ -4,8 +4,10 @@ import pytest
 
 from lanarky.register import (
     STREAMING_CALLBACKS,
+    STREAMING_JSON_CALLBACKS,
     WEBSOCKET_CALLBACKS,
     register_streaming_callback,
+    register_streaming_json_callback,
     register_websocket_callback,
 )
 from lanarky.register.base import register
@@ -25,6 +27,12 @@ def test_register(registry: Dict[str, Tuple[Any, List[str]]]):
 
     with pytest.raises(KeyError):
         register("my_key", registry)(cls=MyClass)
+
+    class MyOtherClass:
+        pass
+
+    register("my_key", registry, override=True)(cls=MyOtherClass)
+    assert registry["my_key"] == MyOtherClass
 
 
 def test_register_with_required_kwargs(registry: Dict[str, Tuple[Any, List[str]]]):
@@ -46,6 +54,17 @@ def test_register_streaming_callback(registry: Dict[str, Tuple[Any, List[str]]])
     assert "my_streaming_callback" in STREAMING_CALLBACKS
     assert STREAMING_CALLBACKS["my_streaming_callback"] == MyStreamingCallback
 
+    with pytest.raises(KeyError):
+        register_streaming_callback("my_streaming_callback")(cls=MyStreamingCallback)
+
+    class MyOtherStreamingCallback:
+        pass
+
+    register_streaming_callback("my_streaming_callback", override=True)(
+        cls=MyOtherStreamingCallback
+    )
+    assert STREAMING_CALLBACKS["my_streaming_callback"] == MyOtherStreamingCallback
+
 
 def test_register_websocket_callback(registry: Dict[str, Tuple[Any, List[str]]]):
     class MyWebsocketCallback:
@@ -54,3 +73,44 @@ def test_register_websocket_callback(registry: Dict[str, Tuple[Any, List[str]]])
     register_websocket_callback("my_websocket_callback")(cls=MyWebsocketCallback)
     assert "my_websocket_callback" in WEBSOCKET_CALLBACKS
     assert WEBSOCKET_CALLBACKS["my_websocket_callback"] == MyWebsocketCallback
+
+    with pytest.raises(KeyError):
+        register_websocket_callback("my_websocket_callback")(cls=MyWebsocketCallback)
+
+    class MyOtherWebsocketCallback:
+        pass
+
+    register_websocket_callback("my_websocket_callback", override=True)(
+        cls=MyOtherWebsocketCallback
+    )
+
+    assert WEBSOCKET_CALLBACKS["my_websocket_callback"] == MyOtherWebsocketCallback
+
+
+def test_register_streaming_json_callback(registry: Dict[str, Tuple[Any, List[str]]]):
+    class MyStreamingJsonCallback:
+        pass
+
+    register_streaming_json_callback("my_streaming_json_callback")(
+        cls=MyStreamingJsonCallback
+    )
+    assert (
+        STREAMING_JSON_CALLBACKS["my_streaming_json_callback"]
+        == MyStreamingJsonCallback
+    )
+
+    with pytest.raises(KeyError):
+        register_streaming_json_callback("my_streaming_json_callback")(
+            cls=MyStreamingJsonCallback
+        )
+
+    class MyOtherStreamingJsonCallback:
+        pass
+
+    register_streaming_json_callback("my_streaming_json_callback", override=True)(
+        cls=MyOtherStreamingJsonCallback
+    )
+    assert (
+        STREAMING_JSON_CALLBACKS["my_streaming_json_callback"]
+        == MyOtherStreamingJsonCallback
+    )
