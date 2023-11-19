41b7091774f4d9311c348d4f1ae3acdd6e9976a8
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2023-11-19 22:10:53 +0530

    :recycle: chain_config -> config

--- lanarky/adapters/langchain/responses.py
+++ lanarky/adapters/langchain/responses.py
@@ -15,14 +15,14 @@ class StreamingResponse(_StreamingResponse):
     def __init__(
         self,
         chain: Chain,
-        chain_config: dict[str, Any],
+        config: dict[str, Any],
         *args,
         **kwargs,
     ) -> None:
         super().__init__(*args, **kwargs)
 
         self.chain = chain
-        self.chain_config = chain_config
+        self.config = config
 
     async def stream_response(self, send: Send) -> None:
         await send(
@@ -33,15 +33,15 @@ class StreamingResponse(_StreamingResponse):
             }
         )
 
-        if "callbacks" in self.chain_config:
-            for callback in self.chain_config["callbacks"]:
+        if "callbacks" in self.config:
+            for callback in self.config["callbacks"]:
                 if hasattr(callback, "send"):
                     callback.send = send
 
         try:
             # TODO: migrate to `.ainvoke` when adding support
             # for LCEL
-            outputs = await self.chain.acall(**self.chain_config)
+            outputs = await self.chain.acall(**self.config)
             if self.background is not None:
                 self.background.kwargs.update({"outputs": outputs})
         except Exception as e:
