a938bedb4118db6712f98c6fe4d47bafb3b9d57d
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2023-11-20 12:22:37 +0530

    :recycle: WebsocketManager -> WebsocketSession

--- lanarky/websockets.py
+++ lanarky/websockets.py
@@ -3,38 +3,30 @@ from enum import Enum
 
 from fastapi.websockets import WebSocket, WebSocketDisconnect
 
+from lanarky.logging import logger
+
 
 class DataMode(str, Enum):
+    JSON = "json"
     TEXT = "text"
     BYTES = "bytes"
-    JSON = "json"
 
 
-class WebsocketManager:
-    def __init__(self) -> None:
-        self.active_sessions: list[WebSocket] = []
-
+class WebsocketSession:
     @asynccontextmanager
-    async def connect(self, websocket: WebSocket, mode: DataMode = DataMode.TEXT):
-        await self.accept(websocket)
+    async def connect(self, websocket: WebSocket, mode: DataMode = DataMode.JSON):
+        await websocket.accept()
         try:
-            if mode == DataMode.TEXT:
+            if mode == DataMode.JSON:
+                yield self.iter_json(websocket)
+            elif mode == DataMode.TEXT:
                 yield self.iter_text(websocket)
             elif mode == DataMode.BYTES:
                 yield self.iter_bytes(websocket)
-            elif mode == DataMode.JSON:
-                yield self.iter_json(websocket)
             else:
                 raise ValueError(f"Invalid DataMode: {mode}")
         except WebSocketDisconnect:
-            self.disconnect(websocket)
-
-    async def accept(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_sessions.append(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_sessions.remove(websocket)
+            logger.info("Websocket disconnected")
 
     async def iter_text(self, websocket: WebSocket):
         while True:
