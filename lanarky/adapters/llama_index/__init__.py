16c7c98a21a0285bf631c02d812caeea9a7cf448
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2023-11-24 20:59:41 +0530

    :fire: remove llama-index adapter
    insufficient support for v0.8 release. add support in future release

--- lanarky/adapters/llama_index/__init__.py
+++
@@ -1,6 +0,0 @@
-from importlib.util import find_spec
-
-if not find_spec("llama_index"):
-    raise ImportError(
-        "run `pip install lanarky[llama-index]` to use the llama-index adapter"
-    )
