9462b79e38e2cc2c876ebce6a5fa7f3485cf7e9c
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2023-11-26 23:14:08 +0530

    :sparkles: add WebSocketClient
    :wrench: update dependencies

--- lanarky/clients.py
+++ lanarky/clients.py
@@ -1,7 +1,12 @@
-from typing import Generator, Optional
+import json
+from contextlib import contextmanager
+from typing import Any, Generator, Optional
 
 import httpx
 from httpx_sse import connect_sse
+from websockets.sync.client import connect as websocket_connect
+
+from lanarky.websockets import DataMode
 
 
 class StreamingClient:
@@ -18,3 +23,40 @@ class StreamingClient:
         with connect_sse(self.client, method, url, **kwargs) as event_source:
             for sse in event_source.iter_sse():
                 yield sse
+
+
+class WebSocketClient:
+    def __init__(
+        self, uri: str = "ws://localhost:8000/ws", mode: DataMode = DataMode.JSON
+    ):
+        self.uri = uri
+        self.mode = mode
+        self.websocket = None
+
+    @contextmanager
+    def connect(self):
+        with websocket_connect(self.uri) as websocket:
+            self.websocket = websocket
+            yield self
+            self.websocket = None
+
+    def send(self, message: Any):
+        if self.websocket:
+            if self.mode == DataMode.JSON:
+                message = json.dumps(message)
+            elif self.mode == DataMode.TEXT:
+                message = str(message)
+            elif self.mode == DataMode.BYTES:
+                message = message.encode("utf-8")
+            self.websocket.send(message)
+
+    def receive(self):
+        if self.websocket:
+            response = self.websocket.recv()
+            if self.mode == DataMode.JSON:
+                response = json.loads(response)
+            elif self.mode == DataMode.TEXT:
+                response = str(response)
+            elif self.mode == DataMode.BYTES:
+                response = response.decode("utf-8")
+            return response
